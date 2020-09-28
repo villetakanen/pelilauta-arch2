97a8a89d239651a95f9d9665d3a52f9d1a10dde6
Ville Takanen <vitkukissa@gmail.com>
2020-09-28 14:46:56 +0300

    feat: base styling scaffold in place

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>Hello!..</div>
+  <AppBar />
+  <div style="margin-top:72px">Hello!..</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppBar from './components/AppBar.vue'
+
+export default defineComponent({
+  components: {
+    AppBar
+  }
+})
+</script>
 
 <style lang="css">
 @import url("./css/base.css");
---
+++ src/components/AppBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="AppBar">
+    <h1 class="font-button">Pelilauta</h1>
+  </div>
+</template>
