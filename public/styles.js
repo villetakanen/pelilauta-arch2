aada8d43a32600dcdc09cd4acb8950c35be64a21
Ville Takanen <vitkukissa@gmail.com>
2020-09-28 21:36:08 +0300

    feat: minimized styles.js

--- public/styles.js
+++ public/styles.js
@@ -1,8 +1,1 @@
-const code = "\n/* dummy */\n";
-
-const styleEl = document.createElement("style");
-const codeEl = document.createTextNode(code);
-styleEl.type = 'text/css';
-
-styleEl.appendChild(codeEl);
-document.head.appendChild(styleEl);
+const c = ""
