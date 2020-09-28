8992d36911b45146bdf0ec3b9706faa7c850838f
Ville Takanen <vitkukissa@gmail.com>
2020-09-28 23:36:15 +0300

    feat: button component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <AppBar />
+  <MaterialButton
+    fab
+  >
+    +
+  </MaterialButton>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import AppBar from './components/AppBar.vue'
+import MaterialButton from './components/MaterialButton.vue'
 
 export default defineComponent({
   components: {
-    AppBar
+    AppBar,
+    MaterialButton
   }
 })
 </script>
-
-<style>
-/* dummy */
-</style>
