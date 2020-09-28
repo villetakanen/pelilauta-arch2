c9e161566302c834adf10f81ea1326617a438641
Ville Takanen <vitkukissa@gmail.com>
2020-09-28 13:59:55 +0300

    Revert "feat: prettier + eslint"
    This reverts commit 990d4c0e4289d80d5fc767a392433b90fe52726f.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-app.mount('#app')
+const app = createApp(App);
+app.mount("#app");
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot.accept()
+  import.meta.hot.accept();
   import.meta.hot.dispose(() => {
-    app.unmount("#app")
-  })
+    app.unmount();
+  });
 }
