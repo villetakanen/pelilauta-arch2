7f4a67b5cb414fdcce1acb52f1ad3834961dadaa
Ville Takanen <vitkukissa@gmail.com>
2020-09-29 13:47:56 +0300

    feat: login

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import firebase from 'firebase/app'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import NotFound from './views/NotFound.vue'
@@ -11,6 +12,33 @@ const routes = [
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
 const router = createRouter({ history: createWebHistory(), routes })
+
+interface FirebaseEnv {
+  SNOWPACK_PUBLIC_FIREBASE_API_KEY: string,
+  SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN: string,
+  SNOWPACK_PUBLIC_FIREBASE_DATASE_URL: string,
+  SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID: string
+  SNOWPACK_PUBLIC_FIREBASE_STORAGE_BUCKET: string,
+  SNOWPACK_PUBLIC_FIREBASE_MESSAGE_SENDER_ID: string,
+  SNOWPACK_PUBLIC_FIREBASE_APP_ID: string
+}
+
+const env:FirebaseEnv = import.meta.env as FirebaseEnv
+
+const config = {
+  apiKey: env.SNOWPACK_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.SNOWPACK_PUBLIC_FIREBASE_DATASE_URL,
+  projectId: env.SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.SNOWPACK_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.SNOWPACK_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: env.SNOWPACK_PUBLIC_FIREBASE_APP_ID
+}
+firebase.initializeApp(config)
+
+firebase.auth().onAuthStateChanged((user) => {
+  console.log('authStateChanged', user)
+})
 
 const app = createApp(App)
 app.use(router)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="main-view">
-    Login
-    <div>
-      <MaterialButton class="googleLoginButton">
+    <p>Please login or register to Pelilauta with the buttons below.</p>
+    <div style="text-align: center">
+      <MaterialButton
+        :action="socialGoogleLogin"
+        class="googleLoginButton"
+      >
         <img
           src="../assets/google.svg"
           alt="G"
@@ -10,13 +13,16 @@
         > Login with Google
       </MaterialButton>
     </div>
-    <div>
-      <MaterialButton class="facebookLoginButton">
+    <div style="text-align: center">
+      <MaterialButton
+        :action="socialFacebookLogin"
+        class="facebookLoginButton"
+      >
         <img
           src="../assets/facebook.svg"
           alt="F"
           class="icon"
-        > Login with GoogleLogin with Facebook
+        > Login with Facebook
       </MaterialButton>
     </div>
   </div>
@@ -24,11 +30,32 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import MaterialButton from '../components/MaterialButton.vue'
 
 export default defineComponent({
   components: {
     MaterialButton
+  },
+  setup () {
+    const router = useRouter()
+    if (firebase.auth().currentUser?.email) router.push('/')
+
+    function socialGoogleLogin () {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      provider.addScope('email')
+      firebase.auth().signInWithPopup(provider)
+    }
+
+    function socialFacebookLogin () {
+      const provider = new firebase.auth.FacebookAuthProvider()
+      provider.addScope('email')
+      firebase.auth().signInWithPopup(provider)
+    }
+
+    return { socialGoogleLogin, socialFacebookLogin }
   }
 })
 </script>
@@ -38,9 +65,11 @@ export default defineComponent({
 .googleLoginButton
   background-color: white
   color: #666
+  width: 230px
 
 .facebookLoginButton
   background-color: #4267B2
   color: rgba(255,255,255,0.8)
+  width: 230px
 
 </style>
