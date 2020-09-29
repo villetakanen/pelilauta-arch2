d19054a92afbcc254b8ab339bfb778e8983db15f
Ville Takanen <vitkukissa@gmail.com>
2020-09-29 09:15:11 +0300

    feat: login button added

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,5 +1,27 @@
 <template>
-  <div id="AppBar">
-    <h1 class="font-button">Pelilauta</h1>
+  <div
+    id="AppBar"
+  >
+    <div
+      id="AppBarToolbar"
+      class="toolbar"
+    >
+      <h1 class="font-button">
+        Pelilauta
+      </h1>
+      <div class="spacer" />
+      <ProfileIcon />
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ProfileIcon from './ProfileIcon.vue'
+
+export default defineComponent({
+  components: {
+    ProfileIcon
+  }
+})
+</script>
